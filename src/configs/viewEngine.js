979677aac5a83dd9c578b77c1a16f1c11145f0e1
Thanh Tuan <tuandtse243@gmail.com>
2022-11-09 17:37:50 +0700

    su dung bien env

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const configViewEngine = (app) => {
+  app.use(express.static("./src/public")); //cấu hình cho file nào có thể được xem từ kết nối ở bên ngoài, tất cả nằm trong folder public
   //cấu hình express
   app.set("view engine", "ejs"); // cấu hình view engine của express là ejs
   app.set("views", "./src/views"); //cấu hình tất cả màn hình ở trong thư mục views để express vô đây tìm
